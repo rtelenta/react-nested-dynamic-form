2b677d096f42e3361156c4d2a32a7e680c36e060
Renzo Telenta <renzotelenta@gmail.com>
2021-02-10 17:02:48 -0500

    feat(actionButtons): update button html to component

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -1,12 +1,9 @@
+import Button from "./Button";
+
 const ActionButtons: React.FC = () => {
   return (
     <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-      <button
-        type="submit"
-        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Save
-      </button>
+      <Button type="submit">Save</Button>
     </div>
   );
 };
